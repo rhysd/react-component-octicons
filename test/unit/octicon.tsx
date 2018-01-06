b11d28f8d321a182545dab51432214ca51e5f0dd
rhysd <lin90162@yahoo.co.jp>
2018-01-06 20:10:52 +0900

    add unit tests for <Octicon/>

--- test/unit/octicon.tsx
+++ test/unit/octicon.tsx
@@ -1,7 +1,24 @@
 import * as assert from 'assert';
+import * as React from 'react';
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+import Octicon, { OcticonSymbol } from '../../index';
 
-describe('test', function() {
-    it('failed', function() {
-        assert.equal(1, 2);
-    });
+Enzyme.configure({ adapter: new Adapter() });
+
+const { shallow } = Enzyme;
+const symbols = Object.getOwnPropertyNames(require('octicons')) as OcticonSymbol[];
+
+describe('<Octicon>', function() {
+    for (const symbol of symbols) {
+        it(`should render SVG icon '${symbol}'`, function() {
+            const w = shallow(<Octicon name={symbol} />);
+            const svg = w.find('svg');
+            assert.equal(svg.length, 1);
+            const props = svg.first().props();
+            assert.ok(props.width);
+            assert.ok(props.height);
+            assert.ok(props.viewBox);
+        });
+    }
 });
