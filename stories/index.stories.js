f77e8f851b142286f7e9d43cc86fbbd67fabddd0
rhysd <lin90162@yahoo.co.jp>
2018-01-24 04:56:29 +0900

    add 'fork me on GitHub' ribbon

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,11 +1,5 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-
-import { Button, Welcome } from '@storybook/react/demo';
-
 import * as octicons from 'octicons';
 
 // XXX:
@@ -68,8 +62,20 @@ const StylesTable = props => (
     />
 );
 
+const ForkMeOnGitHubRibbon = (
+    <a href="https://github.com/rhysd/react-component-octicons">
+        <img
+            style={{ position: 'absolute', top: '0', right: '0', border: '0' }}
+            src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67"
+            alt="Fork me on GitHub"
+            data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"
+        />
+    </a>
+);
+
 const IconDemo = props => (
     <main>
+        {ForkMeOnGitHubRibbon}
         <h2 className="samples-title">Sized Icons</h2>
         <SizesTable name={props.name} />
         <h2 className="samples-title">Styled Icons</h2>
@@ -77,9 +83,7 @@ const IconDemo = props => (
     </main>
 );
 
-const stories = storiesOf('Octicons', module);
+const stories = storiesOf('Octicons Catalog', module);
 for (const name of Object.keys(octicons)) {
     stories.add(name, () => <IconDemo name={name} />);
 }
-// .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-// .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
